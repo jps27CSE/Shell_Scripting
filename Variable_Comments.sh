f846475457a53d0d77a5f45b43927ed397f14e3e
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2022-05-23 23:40:20 +0600

    Add files via upload

---
+++ Variable_Comments.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+echo "Testing"
+
+echo $BASH 
+echo $BASH_VERSION #Shows Bash Version
+echo $HOME #Shows Home directory
+echo $PWD #Shows present directory
+
+name=jack
+
+echo my name is $name
